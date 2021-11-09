5d2c23b29207ff29f87b4abd457ae66d46b2a78f
Thelma_Lim <dusgud711@ewhain.net>
2021-11-09 17:58:21 +0900

    javascript 연습

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,36 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+// Adding an image changer
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src','images/firefox2.png');
+    } else {
+        myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
+
+// Adding a personalized welcome message
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('please enter your name.');
+    if(!myName){
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+}
